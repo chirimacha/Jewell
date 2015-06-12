5787c6d70c6af10a6e1e73883d7fc1c1e9f318ce
Sasha Gutfraind <sasha@uptake.io>
2015-06-12 12:08:18 -0500

    Update write_search_plan.R

--- write_search_plan.R
+++ write_search_plan.R
@@ -23,9 +23,11 @@ topNode = newXMLNode("top_node")
 house1 = newXMLNode("house1", parent=topNode, attrs=c(address="Calle Grau 1", risk="High", lat=-10, long=30))
 house2 = newXMLNode("house2", parent=topNode, attrs=c(address="Calle Grau 10", risk="High", lat=-10, long=30.1))
 
-house1.1 = newXMLNode("house1_inspected_house1", parent=house1, attrs=c(address="Calle Franklin 42", risk="High", lat=-10, long=30.2))
-house1.2 = newXMLNode("house1_inspected_house2", parent=house1, attrs=c(address="Calle Franklin 5", risk="High", lat=-10, long=30.21))
-house1.3 = newXMLNode("house1_inspected_house3", parent=house1, attrs=c(address="Calle Franklin 6", risk="Medium", lat=-10, long=30.21))
+house1.1 = newXMLNode("house1_inspected__house4", parent=house1, attrs=c(address="Calle Franklin 42", risk="High", lat=-10, long=30.2))
+house1.2 = newXMLNode("house1_inspected__house5", parent=house1, attrs=c(address="Calle Franklin 5", risk="High", lat=-10, long=30.21))
+house1.3 = newXMLNode("house1_inspected__house6", parent=house1, attrs=c(address="Calle Franklin 6", risk="Medium", lat=-10, long=30.21))
+house2.1 = newXMLNode("house2_inspected__house7", parent=house2, attrs=c(address="Calle Jefferson 2", risk="High", lat=-10, long=30.2))
+#the node name indicates the branching structure, so that houseX_inspected is the condition, and now you go to houseY
 
 inspectionPlanText <- saveXML(topNode)
 cat(inspectionPlanText)
