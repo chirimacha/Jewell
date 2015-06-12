a5789dadaa04a84b7ba0279654c2df18de195575
Sasha Gutfraind <sasha@uptake.io>
2015-06-12 11:50:41 -0500

    Create write_search_plan.R

---
+++ write_search_plan.R
@@ -0,0 +1,33 @@
+#example of generation of search plan
+#run this code, and then open with your web browser the file
+fpath = "/tmp/plan.xml"
+
+#more docs on XML
+#http://127.0.0.1:19979/library/XML/html/newXMLDoc.html
+#http://www.omegahat.org/RSXML/shortIntro.pdf
+
+
+requiredPackages <- c("XML"
+                      #,"doParallel"
+                      )
+packages <- requiredPackages[!(requiredPackages %in% rownames(installed.packages()))]
+if(length(packages) > 0) {
+  install.packages(packages)
+}
+invisible(lapply(requiredPackages, require, character.only=T))
+
+#doc = newXMLDoc()
+doc = newXMLDoc(dtd = "", namespaces=NULL, addFinalizer = TRUE, 
+                name = "chiri search plan", node = NULL, isHTML = FALSE)
+topNode = newXMLNode("top_node")
+house1 = newXMLNode("house1", parent=topNode, attrs=c(address="Calle Grau 1", risk="High", lat=-10, long=30))
+house2 = newXMLNode("house2", parent=topNode, attrs=c(address="Calle Grau 10", risk="High", lat=-10, long=30.1))
+
+house1.1 = newXMLNode("house1_inspected_house1", parent=house1, attrs=c(address="Calle Franklin 42", risk="High", lat=-10, long=30.2))
+house1.2 = newXMLNode("house1_inspected_house2", parent=house1, attrs=c(address="Calle Franklin 5", risk="High", lat=-10, long=30.21))
+house1.3 = newXMLNode("house1_inspected_house3", parent=house1, attrs=c(address="Calle Franklin 6", risk="Medium", lat=-10, long=30.21))
+
+inspectionPlanText <- saveXML(topNode)
+cat(inspectionPlanText)
+
+write(inspectionPlanText, file=fpath)
