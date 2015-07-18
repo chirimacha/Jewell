229d21d25df9cb8526fe2f5e6e85d876ba17ebc1
Sasha Gutfraind <sasha.gutfraind@uptake.com>
2015-07-17 22:02:59 -0500

    bug fix

--- code/run_bandit.R
+++ code/run_bandit.R
@@ -1,5 +1,7 @@
 #
 #
+options(warn=2)
+
 DOCUMENTATION <- '
 
 1. def_params below contains configuration of the software
@@ -7,13 +9,17 @@ DOCUMENTATION <- '
 2. use command line parameters to change the parameters at will.  
 e.g. 
 Rscript run_bandit.R --exec_mode="initialize"  --arm_names=TYABAYA,MELGAR         
-Rscript run_bandit.R --exec_mode="update"      --arm=TYABAYA --rewards=0,0,0,1,0 --last_state_path="data/bandit_2015-07-02.csv" 
+Rscript run_bandit.R --exec_mode="update"      --arm=TYABAYA --bugs=0,0,0,1,0 --last_state_path="data/bandit_2015-07-02.csv" 
 
 3. Notes:
 * arm_names and rewards should be listed WITHOUT SPACES
+* generally, last_state_path and output_path should be different files like
+--last_state_path=Bandit2015_07_17.RSave
+--output_path=Bandit2015_07_18.RSave
+* if output_path is not specified, the file last_state_path would NOT be updated, instead a new file will be created.
 * you can revise the same file by controlling the output_path:
 Rscript run_bandit.R --exec_mode="initialize"  --arm_names="TYABAYA","MELGAR" --output_path=output/my_bandit.Rsave
-Rscript run_bandit.R --exec_mode="update"      --rewards=0,0,0,1,10,0  --arm=TYABAYA  --last_state_path=output/my_bandit.Rsave --output_path=output/my_bandit.Rsave
+Rscript run_bandit.R --exec_mode="update"      --bugs=0,0,0,1,2.1,0  --arm=TYABAYA  --last_state_path=output/my_bandit.Rsave --output_path=output/my_bandit.Rsave
 
 
 Details with -h flag
@@ -50,8 +56,8 @@ bandit_initialize <- function(params) {
   #TODO: adjust the prior probabilities
   #TODO: adjust the parameters
   arm_names      <- params$arm_names  
-  
-  scores <- data.frame(site_num=integer(), arm=integer(), arm_name=character(), reward=double(), total_reward=double())
+
+  scores <- data.frame(site_num=integer(), arm=integer(), arm_name=character(), bugs=double(), reward=double(), total_reward=double())
   
   recommended_arm <- next_arm_rc(bandit)
   cat("\n")
@@ -82,8 +88,13 @@ bandit_update <- function(params) {
   arm_names <- bandit_state[["arm_names"]]
   
   arm_idx <- which(arm_names == params$arm)
+  if(length(arm_idx)==0) {
+    cat("Arm: ", params$arm, "is not recognized!\n.  Use one of:", arm_names)
+    cat("Existing!\n")
+    q()
+  }
   scores  <- bandit_state[["scores"]]
-  num_sites   <- length(params$rewards)
+  num_sites   <- length(params$bugs)
   
   site_num <- dim(scores)[1] + 1
   if(site_num == 1) {
@@ -93,14 +104,17 @@ bandit_update <- function(params) {
   }
   for (trial in 1:num_sites) {
     cat("trial: ", trial, "\n")
-    bugs     <- as.numeric(params$rewards[trial]) 
-    reward <-log10(1+bugs) #we set rewards as log10 bugs
-    cat("  arm: ", params$arm, "  reward", reward, "\n")
+    bugs     <- as.numeric(params$bugs[trial]) 
+    reward   <- log10(1+bugs) #IMPORTANT: we set rewards as log10(1+bugs)
+    cat("  arm: ", params$arm, "  bugs", bugs, "  reward", reward, "\n")
     bandit <- update_bandit(bandit, arm_idx, reward)
     cat("  preferences:", paste(bandit$preferences), "\n")
 
     total_reward <- total_reward + reward
-    scores <- rbind(scores, list(site_num=site_num, arm=arm_idx, arm_name=params$arm, reward=reward, total_reward=total_reward))
+    scores <- rbind(scores, list(site_num=site_num, arm=arm_idx, arm_name=params$arm, 
+                                 bugs=bugs, reward=reward, total_reward=total_reward))
+    scores$arm_name <- as.character(scores$arm_name)  #works around initialization bug
+
     site_num <- site_num + 1
   }
   
@@ -143,13 +157,22 @@ parse_cmdl <- function(params=def_params, alt_params=list()) {
     'exec_mode',       'm', 1, "character", "One or more from [initialize,update]",
     'arm',             'p', 1, "character", " (update only) Name of arm pulled",
     'arm_names',       'a', 1, "character", " (initialization only) Two or more names of arms like c(\"name1\",\"name2\")",
-    'rewards',         'r', 1, "character", " (update only) Numerical rewards separated by commas (0=nothing found, >0 size of infestation (#bugs))",
+    'bugs',            'b', 1, "character", " (update only) Number of bugs separated by commas (0=nothing found, >0 size of infestation (#bugs))",
     'output_path',     'o', 1, "character", "path for output of result",
     'last_state_path', 'l', 1, "character",  "path to last state of the bandit",
     'verbose',         'v', 2, "integer",   "NOT USED",
     'help'   ,         'h', 0, "logical",   "Writes this message."
   ), byrow=TRUE, ncol=5)
-  opt <- getopt(spec)
+  
+  cmdArgs<-commandArgs(TRUE)
+  #for debugging from Rstudio
+  #cmdArgs<-c('--exec_mode=update', '--arm=MELGAR', '--bugs=0,0,1,0,0,0,0,0,0,0', 
+  #           '--output_path=/tmp/1.rs','--last_state_path=/tmp/1.rs')
+  #cmdArgs<-c('--exec_mode=initialize', '--arm_names=TIABAYA,ASA,MELGAR,HUNTER', 
+  #           '--output_path=/tmp/1.rs')
+  
+
+  opt <- getopt(spec, opt=cmdArgs)
   
   if ( !is.null(opt$exec_mode) ) {
     params[['exec_mode']] <- opt$exec_mode
@@ -169,11 +192,11 @@ parse_cmdl <- function(params=def_params, alt_params=list()) {
   }
   
   if (params$exec_mode == "update") {
-    if ( !is.null(opt$rewards)) {
-      params[['rewards']] <- unlist(strsplit(opt$rewards, ",")[[1]])
-      cat("rewards:", params$rewards, "\n")      
+    if ( !is.null(opt$bugs)) {
+      params[['bugs']] <- unlist(strsplit(opt$bugs, ",")[[1]])
+      cat("bugs:", params$bugs, "\n")      
     } else {
-      print("Warning: missing rewards argument")
+      print("Warning: missing bugs argument")
       q(1)
     }
     if ( !is.null(opt$arm)) {
@@ -229,7 +252,6 @@ def_params <- list(  #overriden by some command line arguments
   some_other_param=NULL
 )
 
-browser()
 params <- parse_cmdl(params=def_params, alt_params=list())
 
 if(params$exec_mode == "initialize") {
