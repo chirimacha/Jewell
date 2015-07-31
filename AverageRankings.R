d256c5401fa0ab7c5b50d4537dbd8c425b433af0
Patrick Emedom-Nnamdi <patricknnamdi2@gmail.com>
2015-07-31 12:57:26 -0400

    Average Rankings
    Average probabilities across the three chains into a single csv

--- AverageRankings.R
+++ AverageRankings.R
@@ -1,7 +1,10 @@
+## Set working directory to location of output csv
 setwd("~/Desktop/Levy_Lab/Jewell/SensAnalysis/K.Sens.Results")
-v1 <- data.frame(read.table("100.1000000ResultsKv1"))
-v2 <- data.frame(read.table("100.1000000ResultsKv2"))
-v3 <- data.frame(read.table("100.1000000ResultsKv3"))
+
+## In v1, v2, and v3 insert the name of the csv and run all
+v1 <- data.frame(read.table("ResultsKv1"))
+v2 <- data.frame(read.table("ResultsKv2"))
+v3 <- data.frame(read.table("ResultsKv3"))
 v1 <- v1[order(v1$unicode),]
 v2 <- v2[order(v2$unicode),]
 v3 <- v3[order(v3$unicode),]
@@ -11,4 +14,6 @@ v <- v1[order(v1$probmean, decreasing = TRUE),]
 Ranking <- c(1:dim(v)[1])
 averageRanking <- cbind(v, Ranking)
 averageRanking$occult.prob <- NULL 
+
+## New single csv should be in same folder as working directory 
 write.csv(averageRanking, file="AverageResults",row.names=FALSE)
